70d8710061268a3c81aa990d32e19a58dd2ac328
Djun M Kim <djun.k@myplanetdigital.com>
2014-06-06 10:22:36 -0700

    Refactor to separate pitch detection from audio loading and tuner display

---
+++ js/tunerdemo.js
@@ -0,0 +1,204 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Chris Wilson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+var audioContext = new AudioContext();
+var isPlaying = false;
+var sourceNode = null;
+var analyser = null;
+var theBuffer = null;
+var detectorElem, 
+	canvasContext,
+	pitchElem,
+	noteElem,
+	detuneElem,
+	detuneAmount;
+var WIDTH=300;
+var CENTER=150;
+var HEIGHT=42;
+var confidence = 0;
+var currentPitch = 0;
+
+var rafID = null;
+
+var buflen = 2048;
+var buf = new Uint8Array( buflen );
+
+var MINVAL = 134;  // 128 == zero.  MINVAL is the "minimum detected signal" level.
+
+window.onload = function() {
+	var request = new XMLHttpRequest();
+	request.open("GET", "../sounds/whistling3.ogg", true);
+	request.responseType = "arraybuffer";
+	request.onload = function() {
+	  audioContext.decodeAudioData( request.response, function(buffer) { 
+	    	theBuffer = buffer;
+		} );
+	}
+	request.send();
+
+	detectorElem = document.getElementById( "detector" );
+	pitchElem = document.getElementById( "pitch" );
+	noteElem = document.getElementById( "note" );
+	detuneElem = document.getElementById( "detune" );
+	detuneAmount = document.getElementById( "detune_amt" );
+	canvasContext = document.getElementById( "output" ).getContext("2d");
+
+	detectorElem.ondragenter = function () { 
+		this.classList.add("droptarget"); 
+		return false; };
+	detectorElem.ondragleave = function () { this.classList.remove("droptarget"); return false; };
+	detectorElem.ondrop = function (e) {
+  		this.classList.remove("droptarget");
+  		e.preventDefault();
+		theBuffer = null;
+
+	  	var reader = new FileReader();
+	  	reader.onload = function (event) {
+	  		audioContext.decodeAudioData( event.target.result, function(buffer) {
+	    		theBuffer = buffer;
+	  		}, function(){alert("error loading!");} ); 
+
+	  	};
+	  	reader.onerror = function (event) {
+	  		alert("Error: " + reader.error );
+		};
+	  	reader.readAsArrayBuffer(e.dataTransfer.files[0]);
+	  	return false;
+	};
+
+
+
+}
+
+function error() {
+    alert('Stream generation failed.');
+}
+
+function getUserMedia(dictionary, callback) {
+    try {
+        navigator.getUserMedia = 
+        	navigator.getUserMedia ||
+        	navigator.webkitGetUserMedia ||
+        	navigator.mozGetUserMedia;
+        navigator.getUserMedia(dictionary, callback, error);
+    } catch (e) {
+        alert('getUserMedia threw exception :' + e);
+    }
+}
+
+function gotStream(stream) {
+    // Create an AudioNode from the stream.
+    var mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+    // Connect it to the destination.
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    mediaStreamSource.connect( analyser );
+    updatePitch();
+}
+
+function toggleLiveInput() {
+    getUserMedia({audio:true}, gotStream);
+}
+
+function togglePlayback() {
+    var now = audioContext.currentTime;
+
+    if (isPlaying) {
+        //stop playing and return
+        sourceNode.stop( now );
+        sourceNode = null;
+        analyser = null;
+        isPlaying = false;
+		if (!window.cancelAnimationFrame)
+			window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
+        window.cancelAnimationFrame( rafID );
+        return "start";
+    }
+
+    sourceNode = audioContext.createBufferSource();
+    sourceNode.buffer = theBuffer;
+    sourceNode.loop = true;
+
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    sourceNode.connect( analyser );
+    analyser.connect( audioContext.destination );
+    sourceNode.start( now );
+    isPlaying = true;
+    isLiveInput = false;
+    updatePitch();
+
+    return "stop";
+}
+
+function updatePitch( ) {
+	var cycles = new Array;
+	analyser.getByteTimeDomainData( buf );
+
+	autoCorrelate( buf, audioContext.sampleRate );
+
+	canvasContext.clearRect(0,0,WIDTH,HEIGHT);
+
+ 	if (confidence <10) {
+ 		detectorElem.className = "vague";
+	 	pitchElem.innerText = "--";
+		noteElem.innerText = "-";
+		detuneElem.className = "";
+		detuneAmount.innerText = "--";
+ 	} else {
+	 	detectorElem.className = "confident";
+	 	pitchElem.innerText = Math.floor( currentPitch ) ;
+	 	var note =  noteFromPitch( currentPitch );
+		noteElem.innerHTML = noteStrings[note%12];
+		var detune = centsOffFromPitch( currentPitch, note );
+		if (detune == 0 ) {
+			detuneElem.className = "";
+			detuneAmount.innerHTML = "--";
+
+			// TODO: draw a line.
+		} else {
+			if (Math.abs(detune)<10)
+				canvasContext.fillStyle = "green";
+			else
+				canvasContext.fillStyle = "red";
+
+			if (detune < 0) {
+	  			detuneElem.className = "flat";
+			}
+			else {
+				detuneElem.className = "sharp";
+			}
+  			canvasContext.fillRect(CENTER, 0, (detune*3), HEIGHT);
+			detuneAmount.innerHTML = Math.abs( Math.floor( detune ) );
+		}
+	}
+
+	if (!window.requestAnimationFrame) {
+		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+        }
+	rafID = window.requestAnimationFrame( updatePitch );
+}
